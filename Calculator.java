519cd712c9190202d9fe2718927a1d226768f97d
Jack Hettinger <55885485+jdHettinger@users.noreply.github.com>
2023-11-26 15:41:30 -0500

    Implement input validation

--- Calculator.java
+++ Calculator.java
@@ -1,12 +1,15 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import shapes.Rectangle;
+import shapes.Circle;
+import shapes.Triangle;
 
 class Calculator {
     public static void main(String[] args) {
 
         Scanner console = new Scanner(System.in);
 
-        System.out.println("Hello! \n Welcome to a the shape selector! \n ---------------------- \n ###################### \n ---------------------- \n To get started, please select a shape from the menu: \n 1.) Rectangle \n 2.) Triangle \n 3.) Circle \n Select one of the options by entering one of the digits below: ");
+        System.out.println("Hello! \n Welcome to a the shape selector! \n ---------------------- \n ###################### \n ---------------------- \n To get started, please select a shape from the menu: \n 1.) Rectangle \n 2.) Circle \n 3.) Triangle \n Select one of the options by entering one of the digits below: ");
 
 
         try {
@@ -15,9 +18,9 @@ class Calculator {
                 case 1:
                     double width, length;
                     System.out.println("Enter a width: ");
-                    width = console.nextDouble();
+                    width = validateInput(console.nextDouble());
                     System.out.println("Enter a length: ");
-                    length = console.nextDouble();
+                    length = validateInput(console.nextDouble());
                     Rectangle rectangle = new Rectangle(width, length);
                     System.out.println("Your area of this rectangle is: " + rectangle.calculateArea() + "." + "\n Is your rectangle also a square?: " + rectangle.checkSquare());
                     break;
@@ -25,7 +28,7 @@ class Calculator {
                 case 2:
                     double radius;
                     System.out.println("Enter a radius for the circle: ");
-                    radius = console.nextDouble();
+                    radius = validateInput(console.nextDouble());
                     Circle newCircle = new Circle(radius);
                     System.out.println("Your circle area is: " + newCircle.calculateArea());
                     break;
@@ -33,9 +36,9 @@ class Calculator {
                 case 3:
                     double height, base;
                     System.out.println("Enter a height for the triangle: ");
-                    height = console.nextDouble();
+                    height = validateInput(console.nextDouble());
                     System.out.println("Enter a base: ");
-                    base = console.nextDouble();
+                    base = validateInput(console.nextDouble());
                     Triangle newTriangle = new Triangle(height, base);
                     System.out.println("Your triangle area is: " + newTriangle.calculateArea());
                     break;
@@ -45,10 +48,16 @@ class Calculator {
                     break;
             }
         } catch (InputMismatchException e){
-            System.out.println("Improper input! Not a number! Error! Error! Error!");
-            System.out.println(e.getCause());
+            System.out.println("Improper input! Not a number! Error! Error! Error!" + e.getLocalizedMessage());
         }
 
         console.close();
+    }
+
+    private static double validateInput(double value){
+        if(value <= 0){
+            throw new IllegalArgumentException("Not a valid input!");
+        }
+        return value;
     }
 }
